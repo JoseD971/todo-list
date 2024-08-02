1a72e42a4014ca806f8e2dbd0cf16ab5ca86a161
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2024-08-02 15:54:22 -0500

    Carga de plantilla para cada botón de filtro
    display.js: Función 'template' exclusiva para el cargue de la plantilla. Funciones personalizadas para cada botón de filtro.

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,24 +1,35 @@
 import ToDo from './todo.js';
+import Modal from './modal';
 import Project from './project.js';
 
 const Display = (() => {
 
-    const init = () => {
-        const content = document.getElementById('content');
-        const sortToday = document.getElementById('sort-today');
+    const content = document.getElementById('content');
 
-        sortToday.addEventListener('click', today);
+    const init = () => {
+        const showAll = document.getElementById('show-all');
+        const showToday = document.getElementById('show-today');
+        const showWeek = document.getElementById('show-week');
+        const showImportant = document.getElementById('show-important');
+        const showCompleted = document.getElementById('show-completed');
+
+        showAll.addEventListener('click', all);
+        showToday.addEventListener('click', today);
+        showWeek.addEventListener('click', week);
+        showImportant.addEventListener('click', important);
+        showCompleted.addEventListener('click', completed);
     }
     
-    const today = () => {
+    const template = (title, number) => {
+        content.innerHTML = '';
         content.innerHTML = `
         <div class="main-panel">
                 <div class="header">
-                    <h1>Today</h1>
-                    <span>8</span>
+                    <h1>${title}</h1>
+                    <span>${number}</span>
                 </div>
                 <div class="options">
-                    <button class="new-task"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg> Add new task</button>
+                    <button class="new-task" id="add-todo"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg> Add new task</button>
                     <div class="sort-button">
                         <label for="sort">Sort by</label>
                         <select name="sort" id="sort">
@@ -29,14 +40,32 @@ const Display = (() => {
                     </div>
                 </div>
                 <div id="list">
-                    <div class="card">
-                        <input type="checkbox" name="chckd">
-                        <p>Task number 1</p>
-                        <button title="Expand task" class="expand"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>chevron-right</title><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg></button>
-                    </div>
+                    
                 </div>
             </div>
         `;
+
+        Modal.activate();
+    }
+
+    const all = () => {
+        template('All', 30);
+    }
+
+    const today = () => {
+        template('Today', 5);
+    }
+
+    const week = () => {
+        template('This Week', 12);
+    }
+
+    const important = () => {
+        template('Important', 3);
+    }
+
+    const completed = () => {
+        template('Completed', 6);
     }
 
     return {init}
